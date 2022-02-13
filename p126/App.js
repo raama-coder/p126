35fe2535fd936e768afc316a5f609c261b9a9c62
raama-coder <72999673+raama-coder@users.noreply.github.com>
2022-02-13 17:11:59 -0500

    Add files via upload

---
+++ p126/App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import PickImage from './Screens/camera'
+
+
+export default function App() {
+  return (
+  <PickImage/>
+  );
+}
